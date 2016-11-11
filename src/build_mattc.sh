53c987afc9f0257c5410990ca2a29a3338b686d4
Matthew Taylor <mxt375@bham.ac.uk>
2016-11-11 14:31:54 +0000

    Fixed typo in UNIX buils script

--- src/build_mattc.sh
+++ src/build_mattc.sh
@@ -16,6 +16,6 @@ ocamlc -c mattc_par.ml
 ocamlc -c mattc_lex.ml
 ocamlc -c mattc.ml
 
-ocamlc -o mattc.exe parseTreeType.cmo parseTreeEvaluator.cmo parseTreeOptimiser.cmo instructionSetType.cmo instructionSetConvert.cmo instructionSetEvaluate.cmo mattc_lex.cmo mattc_par.cmo mattc.cmo
+ocamlc -o mattc parseTreeType.cmo parseTreeEvaluator.cmo parseTreeOptimiser.cmo instructionSetType.cmo instructionSetConvert.cmo instructionSetEvaluate.cmo mattc_lex.cmo mattc_par.cmo mattc.cmo
 
 echo Compilation Complete
