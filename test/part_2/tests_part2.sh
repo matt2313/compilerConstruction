f57f8c9802cea6827d3c91c6a72b2eb987f3c38e
Matthew Taylor <matt2313@gmail.com>
2016-11-15 17:56:28 +0000

    Changed build script to a makefile

--- test/part_2/tests_part2.sh
+++ test/part_2/tests_part2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script runs the test code for the MattC compiler
-"../../src/mattc" -e "test_1.mc" "test_2.mc" "test_3.mc" "test_4.mc" "test_5.mc" "test_6.mc" "test_7.mc" "test_8.mc" "test_9.mc" "test_10.mc" "test_fib.mc"
+"../../bin/mattc" -e "test_1.mc" "test_2.mc" "test_3.mc" "test_4.mc" "test_5.mc" "test_6.mc" "test_7.mc" "test_8.mc" "test_9.mc" "test_10.mc" "test_fib.mc"
